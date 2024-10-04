39b1af8741d3faa1077e74819e2dee4db6efed0d
Royal <88213897+RoyalSule@users.noreply.github.com>
2024-10-04 16:57:48 +0000

    improving the class completed

--- rect.cpp
+++ rect.cpp
@@ -1,16 +1,18 @@
 #include <stdexcept>
 #include "rect.hpp"
 
-Rectangle::Rectangle(double x, double y, double w, double h):
-    xorigin(x), yorigin(y), width(w), height(h)
-{
-    if (width <= 0 || height <= 0) {
-        throw std::invalid_argument("dimensions must be > 0");
-    }
-}
+using namespace std;
 
 Rectangle::Rectangle(double w, double h) :
     xorigin(0), yorigin(0), width(w), height(h)
+{
+    if (width <= 0 || height <= 0) {
+        throw invalid_argument("dimensions must be > 0\n");
+    }
+}
+
+Rectangle::Rectangle(double x, double y, double w, double h):
+    xorigin(x), yorigin(y), width(w), height(h)
 {
 }
 
--- testrect.cpp
+++ testrect.cpp
@@ -1,16 +1,22 @@
+#include <stdexcept>
 #include <iostream>
 #include "rect.hpp"
 
 using namespace std;
 
 int main() {
-    Rectangle r(40, 25);
+    try {
+        Rectangle r(0, 0);
 
-    cout << "Origin = (" << r.getX() << ", " << r.getY() << ")"
-         << "\nWidth = " << r.getWidth()
-         << "\nHeight = " << r.getHeight()
-         << "\nArea = " << r.area()
-         << "\nPerimeter = " << r.perimeter() << endl;
+        cout << "Origin = (" << r.getX() << ", " << r.getY() << ")"
+             << "\nWidth = " << r.getWidth()
+             << "\nHeight = " << r.getHeight()
+             << "\nArea = " << r.area()
+             << "\nPerimeter = " << r.perimeter() << endl;
+    }
+    catch(const exception& e) {
+        cerr << e.what();
+    }
 
     return 0;
 }
